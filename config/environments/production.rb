b26a04511b3c6340ede1a4e57652fab5c0bb8a15
Vikash Radia <vikash@me.com>
2013-11-18 23:20:24 +0800

    Implemented devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ Mycity::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # Required for Heroku
+  # Set this to actual host
+  config.action_mailer.default_url_options = { :host => 'http://mycity-commits.herokuapp.com/' }
 end
