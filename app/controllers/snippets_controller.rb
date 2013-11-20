fcc1163d117e53dd8104fad0a9c7077862aab8d0
Vikash Radia <vikash@me.com>
2013-11-20 13:47:58 +0800

    Added pagination to pins index

--- app/controllers/snippets_controller.rb
+++ app/controllers/snippets_controller.rb
@@ -5,7 +5,7 @@ class SnippetsController < ApplicationController
   # GET /snippets
   # GET /snippets.json
   def index
-    @snippets = Snippet.all.order("created_at DESC")
+    @snippets = Snippet.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 10)
   end
 
   # GET /snippets/1
