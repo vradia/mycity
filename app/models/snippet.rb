14afb53af8a17b99196d395675d07eb8679380c6
Vikash Radia <vikash@me.com>
2013-11-20 14:14:05 +0800

    Add names to users

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -2,6 +2,10 @@ class Snippet < ActiveRecord::Base
 	belongs_to :user
 
     has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.jpg"
+
+    validates :image, presence: true
+  	validates :description, presence: true
+
 end
 
 
